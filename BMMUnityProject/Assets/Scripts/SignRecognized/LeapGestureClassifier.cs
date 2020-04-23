c9a3beec21448e4ec9cc1a833038ba5913256a3b
ChenJG <dlvguo@qq.com>
2020-04-24 00:17:40 +0800

    up

--- BMMUnityProject/Assets/Scripts/SignRecognized/LeapGestureClassifier.cs
+++ BMMUnityProject/Assets/Scripts/SignRecognized/LeapGestureClassifier.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeapClassify : MonoBehaviour
+public class LeapGestureClassifier : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
